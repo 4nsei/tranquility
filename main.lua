6beda76761d9591d7ff1a4bebde45670f965fbe5
4nsei <cardealerdoesron@gmail.com>
2025-05-04 16:37:37 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
--- getting variables knocked out
+--[[
+getting vars later
+yes if u cant tell this is FilteringDisabled by x0r4ev3r and HerbertV1 by LuaGunsX but my version
+]]--
 local network = game:GetService("NetworkClient")
--- ill add more important vars later
+local oldnet = network:FindFirstChild("ReplicationSettings")
