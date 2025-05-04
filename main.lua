3c3b8b7db18684747e1c86aff112fe7bcd464899
4nsei <cardealerdoesron@gmail.com>
2025-05-04 16:53:42 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,3 +4,7 @@ yes if u cant tell this is FilteringDisabled by x0r4ev3r and HerbertV1 by LuaGun
 ]]--
 local network = game:GetService("NetworkClient")
 local oldnet = network:FindFirstChild("ReplicationSettings")
+
+local function generateAuthTicket()
+  -- ill add code later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+end
